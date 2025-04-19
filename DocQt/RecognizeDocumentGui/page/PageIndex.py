a4c18b877e2355f7b0ea596f83de716b13bd1ccd
FabioPic92 <picciati.fab@gmail.com>
2025-04-19 13:22:36 +0200

    Remove file env

--- DocQt/RecognizeDocumentGui/page/PageIndex.py
+++
@@ -1,5 +0,0 @@
-pageIndex = {
-    "Main": 0, # Pagina dove caricare il documento o aggiungere modello
-    "Analyze": 1, # Pagina con l'immagine analizzata
-    "Result": 2, # Pagina con il risultato del documento analizzato
-}
